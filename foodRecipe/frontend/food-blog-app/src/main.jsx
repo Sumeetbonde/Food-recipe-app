6c644ffad79375faefc2310023396e8aff1e4fb0
Sumeet Bonde <144609220+Sumeetbonde@users.noreply.github.com>
2025-08-10 12:22:57 +0530

    Update main.jsx

--- foodRecipe/frontend/food-blog-app/src/main.jsx
+++ foodRecipe/frontend/food-blog-app/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-const dotenv = require('dotenv').config()
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
